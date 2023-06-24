badac3bc63bd1d12a26eecbe72d2dedeec87ac79
mosmn <ithinkisee8@gmail.com>
2023-06-25 05:45:29 +0800

    created deleteNode, insertNode

--- balancedBST.js
+++ balancedBST.js
@@ -118,7 +118,29 @@ class Tree {
         }
     }
 
-    
+    levelOrder(node) {
+        if(node === null) {
+            return;
+        }
+
+        const queue = [];
+        queue.push(node);
+
+        while(queue.length > 0) {
+            const node = queue.shift();
+            console.log(node.value);
+
+            if(node.left !== null) {
+                queue.push(node.left);
+            }
+
+            if(node.right !== null) {
+                queue.push(node.right);
+            }
+        }
+    }
+
+
 
 
 }
@@ -145,4 +167,6 @@ tree.insert(119999);
 tree.delete(23);
 
 console.log(prettyPrint(tree.root));
+
+tree.levelOrder(tree.root);
 
